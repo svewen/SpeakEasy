2968d6cdf3d6d3a7d1cab583501b42b85c16594a
svewen <116942580+svewen@users.noreply.github.com>
2025-04-07 18:03:45 -0700

    build environment for tutorial scene

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform groundCheck;
+    Vector3 velocity;
+
+    public float speed = 2f;
+    public float sprintSpeed = 5f;
+    public float walkSpeed = 2f;
+    public float gravity = -9.81f;
+    public float groundDistance = 0.4f;
+    public float jumpHeight = 2f;
+    public LayerMask groundMask;
+    private bool isGrounded;
+
+    private void Update()
+    {
+        // Check if player is on ground
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // Handle Sprinting
+        if (Input.GetKey(KeyCode.LeftShift))
+            speed = sprintSpeed;
+        else
+            speed = walkSpeed;
+
+        // Move based on WASD, etc
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
